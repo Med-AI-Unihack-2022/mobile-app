acd6431155a313e07c9725120e9d7b2e114b178b
Bharath Nair <bharathrajeevnair@gmail.com>
2022-02-26 01:48:26 +1030

    onboarding page, installing libs and routing config

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:medbuddy/onboarding.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +13,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MedBuddy',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +32,14 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'MedBuddy'),
+      builder: EasyLoading.init(),
+      initialRoute: '/onboarding',
+      routes: {
+        // '/': (context) => LoginPage(),
+        '/home': (context) => const MyHomePage(title: 'MedBuddy'),
+        '/onboarding': (context) => OnboardingPage(),
+      },
     );
   }
 }
