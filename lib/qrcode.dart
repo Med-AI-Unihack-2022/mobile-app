58753c526807137d13734a49e32b67d9b967a715
Bharath Nair <bharathrajeevnair@gmail.com>
2022-02-27 02:22:42 +1030

    qr code backend integration

--- lib/qrcode.dart
+++ lib/qrcode.dart
@@ -1,7 +1,12 @@
 // ignore_for_file: deprecated_member_use
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:qrscan/qrscan.dart' as scanner;
+import 'package:http/http.dart' as http;
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'navbar.dart';
 
@@ -17,15 +22,19 @@ GlobalKey<ScaffoldState> _key = GlobalKey();
 class _QRCodePageState extends State<QRCodePage> {
   String status = "";
   String scanResult = "";
+  bool isConnected = false;
+  String docName = "";
 
   @override
   void initState() {
     super.initState();
     status = "Not Connected";
     scanResult = "";
+    docName = "";
   }
 
   void _scanQR() async {
+    final prefs = await SharedPreferences.getInstance();
     setState(() {
       status = "Connecting...";
     });
@@ -33,8 +42,56 @@ class _QRCodePageState extends State<QRCodePage> {
       String? result = await scanner.scan();
       setState(() {
         scanResult = result!;
-        status = "Connected";
+        status = "Verifying connection...";
       });
+      var qrCodeToken = result!.split(",")[1];
+      var userToken = prefs.getString("auth_token");
+      var response = await http.get(Uri.parse(
+          "http://119.91.29.100:8000/qr_codes/verify?patient_token=$userToken&qr_code_token=$qrCodeToken"));
+      print(response.body);
+      var jsonBody = json.decode(response.body);
+      var doctorName = jsonBody["first_name"] + " " + jsonBody["last_name"];
+      setState(() {
+        status = "Awaiting authorisation...";
+      });
+      Alert(
+        context: context,
+        type: AlertType.warning,
+        title: "Connection Request",
+        desc: "MEDBUDDY is requesting permission to connect with $doctorName",
+        buttons: [
+          DialogButton(
+            child: Text(
+              "Accept",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            onPressed: () => {
+              Navigator.pop(context),
+              setState(() {
+                status = "Connected";
+                isConnected = true;
+                doctorName = doctorName;
+                docName = doctorName;
+              })
+            },
+            color: Colors.green,
+          ),
+          DialogButton(
+            child: Text(
+              "Reject",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            onPressed: () => {
+              Navigator.pop(context),
+              setState(() {
+                status = "Not Connected";
+                isConnected = false;
+              })
+            },
+            color: Colors.red,
+          )
+        ],
+      ).show();
     } on Exception catch (e) {
       setState(() {
         status = "Not Connected";
@@ -42,20 +99,49 @@ class _QRCodePageState extends State<QRCodePage> {
     }
   }
 
+  Widget connectedSyncAnimation() {
+    return isConnected ? Image.asset("assets/syncing.gif") : Container();
+  }
+
   Widget scanButton() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10.0),
-      child: RaisedButton(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        onPressed: _scanQR,
-        padding: const EdgeInsets.all(12),
-        color: Colors.black,
-        child: const Text('Scan QR Code',
-            style: TextStyle(color: Colors.white, fontSize: 18)),
-      ),
-    );
+    return isConnected
+        ? Container()
+        : Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              onPressed: _scanQR,
+              padding: const EdgeInsets.all(12),
+              color: Colors.black,
+              child: const Text('Scan QR Code',
+                  style: TextStyle(color: Colors.white, fontSize: 18)),
+            ),
+          );
+  }
+
+  Widget disconnectButton() {
+    return isConnected
+        ? Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              onPressed: () {
+                setState(() {
+                  status = "Not Connected";
+                  isConnected = false;
+                });
+              },
+              padding: const EdgeInsets.all(12),
+              color: Colors.black,
+              child: const Text('Disconnect',
+                  style: TextStyle(color: Colors.white, fontSize: 18)),
+            ),
+          )
+        : Container();
   }
 
   Widget statusText() {
@@ -96,9 +182,20 @@ class _QRCodePageState extends State<QRCodePage> {
           padding: const EdgeInsets.only(left: 24.0, right: 24.0),
           // ignore: prefer_const_literals_to_create_immutables
           children: <Widget>[
+            connectedSyncAnimation(),
+            const SizedBox(height: 30),
             statusText(),
             const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: Text(
+                'Provider: ' + docName,
+                style: const TextStyle(fontSize: 18),
+              ),
+            ),
+            const SizedBox(height: 10),
             scanButton(),
+            disconnectButton(),
           ],
         ),
       ),
