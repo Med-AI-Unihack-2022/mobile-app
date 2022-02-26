3ffd56add6c7f544b29a55ac1a12b377fa4a4655
HenryNg101 <hungnguyenquoc8a2@gmail.com>
2022-02-27 05:22:35 +1030

    Merge conflicted file(s)

---
+++ lib/med_report/med_report_desc.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:medbuddy/med_report/med_report.dart';
+
+class MedReportDesc extends StatefulWidget {
+  final MedReport medReport;
+
+  const MedReportDesc({Key? key, required this.medReport}) : super(key: key);
+
+  @override
+  _MedReportDescState createState() => _MedReportDescState();
+}
+
+class _MedReportDescState extends State<MedReportDesc> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlue,
+          title: Text(
+            widget.medReport.appointmentType + " report",
+          ),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () => Navigator.pop(context),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "Time: " + widget.medReport.time + " " + widget.medReport.date,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontFamily: "Lato",
+                  fontSize: 20,
+                ),
+              ),
+              Text(
+                "Location: " + widget.medReport.location,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontFamily: "Lato",
+                  fontSize: 20,
+                ),
+              ),
+              Text(
+                "Doctor: " + widget.medReport.doctor,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontFamily: "Lato",
+                  fontSize: 20,
+                ),
+              ),
+              if (widget.medReport.checkup.cholesterol != '')
+                Text(
+                  "Cholesterol: " + widget.medReport.checkup.cholesterol,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontFamily: "Lato",
+                    fontSize: 20,
+                  ),
+                ),
+              if (widget.medReport.checkup.diastolic != '')
+                Text(
+                  "Diastolic: " + widget.medReport.checkup.diastolic,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontFamily: "Lato",
+                    fontSize: 20,
+                  ),
+                ),
+              if (widget.medReport.checkup.glucose != '')
+                Text(
+                  "Glucose: " + widget.medReport.checkup.glucose,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontFamily: "Lato",
+                    fontSize: 20,
+                  ),
+                ),
+              if (widget.medReport.checkup.systolic != '')
+                Text(
+                  "Systolic: " + widget.medReport.checkup.systolic,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontFamily: "Lato",
+                    fontSize: 20,
+                  ),
+                ),
+              Text(
+                "Remarks: " + widget.medReport.remarks,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontFamily: "Lato",
+                  fontSize: 20,
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Image.network(widget.medReport.files[index]);
+                  },
+                  itemCount: widget.medReport.files.length,
+                ),
+              ),
+              Text(
+                widget.medReport.diagnosis,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontFamily: "Lato",
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
